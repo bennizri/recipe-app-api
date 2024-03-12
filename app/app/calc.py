68a74bd9a37452430980e6e502fe963e7fbc67d3
bennizri <bennizri22@gmail.com>
2024-03-12 13:09:10 +0200

    adding unit testing

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+calculator function
+"""
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x+y
+
+def subtract(x,y):
+    """Subtract x for y and return the result"""
+    return y-x
+
