c49319dccc71488fe11154036a853b2588a3ae9b
bennizri <bennizri22@gmail.com>
2024-03-12 13:40:42 +0200

    adding unit testing

--- app/app/test.py
+++ app/app/test.py
@@ -1,7 +1,6 @@
 """
 sample test
 """
-
 from django.test import SimpleTestCase
 
 from app import calc
@@ -13,9 +12,11 @@ class CalcTest(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
+
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
+        
         self.assertEqual(res, 5)
