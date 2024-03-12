7a3170b030633d61b07d1b61c716fea62ba2107d
bennizri <bennizri22@gmail.com>
2024-03-12 13:42:00 +0200

    adding unit testing

--- app/app/test.py
+++ app/app/test.py
@@ -8,7 +8,6 @@ from app import calc
 
 class CalcTest(SimpleTestCase):
     """Test the calc module."""
-    
     def test_add_numbers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
@@ -18,5 +17,4 @@ class CalcTest(SimpleTestCase):
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
-        
         self.assertEqual(res, 5)
